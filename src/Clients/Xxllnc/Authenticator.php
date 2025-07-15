ac1f3dca48c8b2e86896f3a7d169f137daf6e48c
Robert Bossaert <contact@robertbossaert.com>
2025-07-15 16:14:59 +0200

    fix: use default jwt authenticator for xxllnc

--- src/Clients/Xxllnc/Authenticator.php
+++ src/Clients/Xxllnc/Authenticator.php
@@ -8,13 +8,4 @@ use OWC\ZGW\Contracts\AbstractTokenAuthenticator;
 
 class Authenticator extends AbstractTokenAuthenticator
 {
-    public function getAuthString(): string
-    {
-        return $this->generateToken();
-    }
-
-    public function generateToken(): string
-    {
-        return $this->credentials->getClientSecret();
-    }
 }
