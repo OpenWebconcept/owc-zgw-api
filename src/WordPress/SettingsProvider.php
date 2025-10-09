3cc1d10f4ca12b81185b739371bdb9e3ca6d0d56
Robert Bossaert <contact@robertbossaert.com>
2025-10-09 19:57:53 +0200

    chore: toggle the conditional cmb2 field with js

--- src/WordPress/SettingsProvider.php
+++ src/WordPress/SettingsProvider.php
@@ -11,121 +11,146 @@ class SettingsProvider extends ServiceProvider
     public function register(): void
     {
         add_action('cmb2_admin_init', [$this, 'addSettingsFields']);
+        add_action('admin_footer', [$this, 'registerSettingsPageScripts']);
     }
 
     public function addSettingsFields(): void
     {
         $options = new_cmb2_box([
-            'id' => 'zgw-api-settings',
-            'title' => 'ZGW API instellingen',
-            'object_types' => ['options-page'],
+                'id' => 'zgw-api-settings',
+                'title' => 'ZGW API instellingen',
+                'object_types' => ['options-page'],
 
-            'option_key' => 'zgw_api_settings',
-            'parent_slug' => 'options-general.php',
-            'capability' => 'manage_options',
+                'option_key' => 'zgw_api_settings',
+                'parent_slug' => 'options-general.php',
+                'capability' => 'manage_options',
         ]);
 
         $clients = $options->add_field([
-            'id' => 'zgw-api-configured-clients',
-            'type' => 'group',
-            'description' => 'Configureer ZGW registers',
-            'options' => [
-                'group_title' => 'ZGW Register {#}',
-                'add_button' => 'Voeg nog een register toe',
-                'remove_button' => 'Verwijder register'
-            ],
+                'id' => 'zgw-api-configured-clients',
+                'type' => 'group',
+                'description' => 'Configureer ZGW registers',
+                'options' => [
+                        'group_title' => 'ZGW Register {#}',
+                        'add_button' => 'Voeg nog een register toe',
+                        'remove_button' => 'Verwijder register'
+                ],
         ]);
 
         $options->add_group_field($clients, [
-            'name' => 'Register naam (uniek)',
-            'id' => 'name',
-            'type' => 'text',
+                'name' => 'Register naam (uniek)',
+                'id' => 'name',
+                'type' => 'text',
         ]);
 
         $options->add_group_field($clients, [
-            'name' => 'Register type',
-            'id' => 'client_type',
-            'type' => 'select',
-            'default' => 'openzaak',
-            'options' => [
-                'openzaak' => 'OpenZaak',
-                'xxllnc' => 'XXLLNC',
-                'rxmission' => 'RxMission',
-                'decosjoin' => 'Decos JOIN',
-                'procura' => 'Procura',
-            ]
+                'name' => 'Register type',
+                'id' => 'client_type',
+                'type' => 'select',
+                'default' => 'openzaak',
+                'options' => [
+                        'openzaak' => 'OpenZaak',
+                        'xxllnc' => 'XXLLNC',
+                        'rxmission' => 'RxMission',
+                        'decosjoin' => 'Decos JOIN',
+                        'procura' => 'Procura',
+                ]
         ]);
 
         $options->add_group_field($clients, [
-            'name' => 'Register versie',
-            'id' => 'client_version',
-            'type' => 'select',
-            'default' => 'pre-150',
-            'options' => [
-                'pre-150' => 'ZGW 1.4.1 of eerder',
-                '150' => 'ZGW 1.5.0',
-                '151' => 'ZGW 1.5.1',
-            ]
+                'name' => 'Register versie',
+                'id' => 'client_version',
+                'type' => 'select',
+                'default' => 'pre-150',
+                'options' => [
+                        'pre-150' => 'ZGW 1.4.1 of eerder',
+                        '150' => 'ZGW 1.5.0',
+                        '151' => 'ZGW 1.5.1',
+                ]
         ]);
 
         $options->add_group_field($clients, [
-            'name' => 'Zaken (ZRC) API endpoint',
-            'desc' => 'Bijvoorbeeld: https://test.openzaak.nl/zaken/api/v1',
-            'attributes' => ['placeholder' => 'https://website.nl'],
-            'protocols' => ['https', 'http'],
-            'id' => 'zrc_endpoint',
-            'type' => 'text_url',
+                'name' => 'Zaken (ZRC) API endpoint',
+                'desc' => 'Bijvoorbeeld: https://test.openzaak.nl/zaken/api/v1',
+                'attributes' => ['placeholder' => 'https://website.nl'],
+                'protocols' => ['https', 'http'],
+                'id' => 'zrc_endpoint',
+                'type' => 'text_url',
         ]);
 
         $options->add_group_field($clients, [
-            'name' => 'Catalogus (ZTC) API endpoint',
-            'desc' => 'Bijvoorbeeld: https://test.openzaak.nl/catalogi/api/v1',
-            'attributes' => ['placeholder' => 'https://website.nl'],
-            'protocols' => ['https', 'http'],
-            'id' => 'ztc_endpoint',
-            'type' => 'text_url',
+                'name' => 'Catalogus (ZTC) API endpoint',
+                'desc' => 'Bijvoorbeeld: https://test.openzaak.nl/catalogi/api/v1',
+                'attributes' => ['placeholder' => 'https://website.nl'],
+                'protocols' => ['https', 'http'],
+                'id' => 'ztc_endpoint',
+                'type' => 'text_url',
         ]);
 
         $options->add_group_field($clients, [
-            'name' => 'Documenten (DRC) API endpoint',
-            'desc' => 'Bijvoorbeeld: https://test.openzaak.nl/documenten/api/v1',
-            'attributes' => ['placeholder' => 'https://website.nl'],
-            'protocols' => ['https', 'http'],
-            'id' => 'drc_endpoint',
-            'type' => 'text_url',
+                'name' => 'Documenten (DRC) API endpoint',
+                'desc' => 'Bijvoorbeeld: https://test.openzaak.nl/documenten/api/v1',
+                'attributes' => ['placeholder' => 'https://website.nl'],
+                'protocols' => ['https', 'http'],
+                'id' => 'drc_endpoint',
+                'type' => 'text_url',
         ]);
 
         $options->add_group_field($clients, [
-            'name' => 'Client identifier',
-            'desc' => 'Laat leeg als het register alleen een API key vereist',
-            'id' => 'client_id',
-            'type' => 'text',
+                'name' => 'Client identifier',
+                'desc' => 'Laat leeg als het register alleen een API key vereist',
+                'id' => 'client_id',
+                'type' => 'text',
         ]);
 
         $options->add_group_field($clients, [
-            'name' => 'Client secret',
-            'id' => 'client_secret',
-            'type' => 'text',
-            'attributes' => ['type' => 'password'],
+                'name' => 'Client secret',
+                'id' => 'client_secret',
+                'type' => 'text',
+                'attributes' => ['type' => 'password'],
         ]);
 
         $options->add_group_field($clients, [
-            'name' => 'Client secret (ZRC)',
-            'id' => 'client_secret_zrc',
-            'type' => 'text',
-            'attributes' => ['type' => 'password'],
-            'show_on_cb' => function (\CMB2_Field $field) {
-                $groups = (array) $field->group->value();
-
-                if (!preg_match('/_(\d+)_client_secret_zrc$/', $field->id(), $matches)) {
-                    return false;
-                }
-
-                $index = (int) $matches[1];
-                $client_type = $groups[$index]['client_type'] ?? '';
-
-                return $client_type === 'decosjoin';
-            },
+                'name' => 'Client secret (ZRC)',
+                'id' => 'client_secret_zrc',
+                'type' => 'text',
+                'attributes' => ['type' => 'password'],
         ]);
+    }
+
+    public function registerSettingsPageScripts(): void
+    {
+        $screen = get_current_screen();
+
+        if ($screen && $screen->id === 'settings_page_zgw_api_settings') {
+            ?>
+            <script>
+                (function($) {
+                    'use strict';
+
+                    function toggleClientSecretZRC($group) {
+                        const clientType = $group.find('[name*="[client_type]"]').val();
+                        const $secretRow = $group.find('[name*="[client_secret_zrc]"]').closest('.cmb-row');
+                        $secretRow.toggle(clientType === 'decosjoin');
+                    }
+
+                    function initializeClientSecrets() {
+                        $('.cmb-repeatable-grouping').each(function() {
+                            toggleClientSecretZRC($(this));
+                        });
+                    }
+
+                    // Initialize when DOM is ready.
+                    $(document).ready(initializeClientSecrets);
+
+                    // Watch for changes to client type fields.
+                    $(document).on('change', '[name*="[client_type]"]', function() {
+                        const $group = $(this).closest('.cmb-repeatable-grouping');
+                        toggleClientSecretZRC($group);
+                    });
+                })(jQuery);
+            </script>
+            <?php
+        }
     }
 }
