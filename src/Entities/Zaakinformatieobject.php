d4fe2a64afacaa0b8143c9d7b09888d4712325b9
Robert Bossaert <contact@robertbossaert.com>
2025-10-27 15:06:37 +0100

    fix: zaakinformatieobject expects url

--- src/Entities/Zaakinformatieobject.php
+++ src/Entities/Zaakinformatieobject.php
@@ -10,7 +10,7 @@ class Zaakinformatieobject extends Entity
         // 'url' => "http://example.com",
         // 'uuid' => "095be615-a8ad-4c33-8e9c-c7612fbf6c9f",
         'informatieobject' => Casts\Lazy\Enkelvoudiginformatieobject::class,
-        'zaak' => Casts\Lazy\Zaak::class,
+        'zaak' => Casts\Url::class,
         // 'aardRelatieWeergave' => "Hoort bij, omgekeerd: kent",
         // 'titel' => "string",
         // 'beschrijving' => "string",
